26fa95830eec66cc52e6c00224e766b6f1cc94b1
a-leut <turbomonkeyninja@gmail.com>
2024-12-09 00:10:23 -0700

    mostly there

--- 2024/9/9.py
+++ 2024/9/9.py
@@ -1,23 +1,15 @@
 def disk_map_to_blocks(disk):
-    d_map = ''
+    d_map = []
     id = 0
     m = 1
     for i, c in enumerate(disk):
         if m == 1:
-            d_map += str(id) * int(c)
+            d_map.append(str(id) * int(c))
             id += 1
         else:
-            d_map += '.' * int(c)
+            d_map.append('.' * int(c))
         m *= -1
-    return d_map
-
-def get_last_disc_block(disc_block):
-    for i, c in enumerate(disc_block[::-1]):
-        if c.isnumeric():
-            return len(disc_block) - i
-        
-s = '009..111...2...333.44.5555.6666.777.88889.'
-print(get_last_disc_block(s))
+    return ''.join(d_map)
             
 def is_valid(disc_block):
     m = 'num'
@@ -33,17 +25,20 @@ def is_valid(disc_block):
     return True
 
 def compact_disk_map(d_map):
-    d_block = disk_map_to_blocks(d_map)
     i = 0
-    while not is_valid(d_block):
-        last_n_index = get_last_disc_block(d_block)
-        first_dot_index = d_block.index('.')
-        l = list(d_block)
-        l[first_dot_index] = l[last_n_index-1]
-        l[last_n_index-1] = '.'
-        d_block = ''.join(l)
-        i += 1
-    return d_block
+    j = len(d_map) - 1
+    final = ''
+    while i != j + 1:
+        if d_map[i].isnumeric():
+            final += d_map[i]
+            i += 1
+        elif d_map[j].isnumeric():
+            final += d_map[j]
+            j -= 1
+            i += 1
+        else:
+            j -= 1
+    return final
 
 def get_checksum(d_block):
     s = 0
@@ -51,10 +46,18 @@ def get_checksum(d_block):
         if c.isnumeric():
             s += i * int(c)
         else:
-            break
+            continue
     return s
 
-
-m = compact_disk_map('2333133121414131402')
+with open('input.txt', 'r') as f:
+    s = f.read()
+# s = '2333133121414131402'
+d_block = disk_map_to_blocks(s)
+with open('dblock.txt', 'w') as f:
+    f.write(d_block)
+m = compact_disk_map(d_block)
+with open('compact.txt', 'w') as f:
+    f.write(m)
+print(m)
 s = get_checksum(m)
 print(s)
