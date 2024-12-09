40341305dacd62aac3ec27fa11552ae69e1a9ce5
a-leut <turbomonkeyninja@gmail.com>
2024-12-09 00:26:25 -0700

    diaster

--- 2024/9/9.py
+++ 2024/9/9.py
@@ -2,14 +2,14 @@ def disk_map_to_blocks(disk):
     d_map = []
     id = 0
     m = 1
-    for i, c in enumerate(disk):
+    for c in disk:
         if m == 1:
-            d_map.append(str(id) * int(c))
+            d_map.extend([id] * int(c))
             id += 1
         else:
-            d_map.append('.' * int(c))
+            d_map.extend(['.'] * int(c))
         m *= -1
-    return ''.join(d_map)
+    return d_map
             
 def is_valid(disc_block):
     m = 'num'
@@ -25,15 +25,16 @@ def is_valid(disc_block):
     return True
 
 def compact_disk_map(d_map):
+    print(d_map)
     i = 0
     j = len(d_map) - 1
-    final = ''
+    final = []
     while i != j + 1:
-        if d_map[i].isnumeric():
-            final += d_map[i]
+        if str(d_map[i]).isnumeric():
+            final.append(d_map[i])
             i += 1
-        elif d_map[j].isnumeric():
-            final += d_map[j]
+        elif str(d_map[j]).isnumeric():
+            final.append(d_map[j])
             j -= 1
             i += 1
         else:
@@ -43,7 +44,7 @@ def compact_disk_map(d_map):
 def get_checksum(d_block):
     s = 0
     for i, c in enumerate(d_block):
-        if c.isnumeric():
+        if str(c).isnumeric():
             s += i * int(c)
         else:
             continue
@@ -51,13 +52,12 @@ def get_checksum(d_block):
 
 with open('input.txt', 'r') as f:
     s = f.read()
-# s = '2333133121414131402'
+# s = '233313312141413140211'
 d_block = disk_map_to_blocks(s)
 with open('dblock.txt', 'w') as f:
-    f.write(d_block)
+    f.write(str(d_block))
 m = compact_disk_map(d_block)
 with open('compact.txt', 'w') as f:
-    f.write(m)
-print(m)
+    f.write(str(m))
 s = get_checksum(m)
 print(s)
